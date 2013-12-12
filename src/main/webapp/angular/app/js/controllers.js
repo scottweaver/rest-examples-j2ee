0b8c86e8f0042e83815323e2c5878eec18b5e363
Scott T Weaver <scott.t.weaver@gmail.com>
2013-12-12 11:58:59 -0600

    Add some quotes stuff.

--- src/main/webapp/angular/app/js/controllers.js
+++ src/main/webapp/angular/app/js/controllers.js
@@ -8,4 +8,14 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+  .controller('QuoteListCtrl', function ($scope) {
+      $scope.quotes = [
+        {'quoteId': '10000',
+         'revision': '1'},
+        {'quoteId': '20000',
+         'revision': '2'},
+        {'quoteId': '30000',
+         'revision': '3'}
+      ];
+    });
